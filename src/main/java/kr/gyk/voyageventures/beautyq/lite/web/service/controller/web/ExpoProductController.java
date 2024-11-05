56d2d9b347b05cd41192666dc8eebc706d90c5c7
giyukim <giyu1020@gmail.com>
2024-11-05 10:30:19 +0900

    v0.0.1-b1

--- src/main/java/kr/gyk/voyageventures/beautyq/lite/web/service/controller/web/ExpoProductController.java
+++ src/main/java/kr/gyk/voyageventures/beautyq/lite/web/service/controller/web/ExpoProductController.java
@@ -3,6 +3,7 @@ package kr.gyk.voyageventures.beautyq.lite.web.service.controller.web;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.gyk.voyageventures.beautyq.lite.web.service.component.CookieComponent;
+import kr.gyk.voyageventures.beautyq.lite.web.service.dto.web.*;
 import kr.gyk.voyageventures.beautyq.lite.web.service.service.web.CosmeticService;
 import kr.gyk.voyageventures.beautyq.lite.web.service.service.web.ExpoProductService;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -55,9 +61,14 @@ public class ExpoProductController {
             @PathVariable(name="id") Long id,
             @PathVariable(name="tag") Short tag
     ) throws Exception {
-        model.addAttribute("productSim", expoProductService.getCosmeticScoreListDTOSimilar(id, tag, cookieComponent.getDiagnosisSkinType(httpServletRequest), cookieComponent.getMainTag(httpServletRequest), cookieComponent.getScoringRandom(httpServletRequest)));
+        CosmeticScoreListDTO cosmeticListDTO = expoProductService.getCosmeticScoreListDTOSimilar(id, tag, cookieComponent.getDiagnosisSkinType(httpServletRequest), cookieComponent.getMainTag(httpServletRequest), cookieComponent.getScoringRandom(httpServletRequest));
+        CosmeticScoreListElementDTO firstDTO = cosmeticListDTO.getCount() > 1 ? cosmeticListDTO.getCosmeticList().getFirst() : null;
+        List<CosmeticScoreListElementDTO> newListElementDTO = cosmeticListDTO.getCount() > 1 ? cosmeticListDTO.getCosmeticList().stream().skip(1).toList() : new ArrayList<>();
 
-        return "product_compare_ajax:: #bq-recommend-content";
+        model.addAttribute("productTop", firstDTO);
+        model.addAttribute("productSim", CosmeticScoreListDTO.builder().count((long) newListElementDTO.size()).cosmeticList(newListElementDTO).build());
+
+        return "product_compare_ajax";
     }
 
 }
